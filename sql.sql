3eaaeaa10b81bc673cf76d739082ffddeaddeec5
charlotosisi <valentin.gaete@pregrado.uoh.cl>
2023-04-11 11:30:58 -0400

    wena los compas

---
+++ sql.sql
@@ -0,0 +1,26 @@
+-- INSERT INTO user(id,name) 
+--     VALUES(1,"Valentin"),
+--             (2,"Cesar");
+-- DELETE INTO user(id,name)
+--     VALUES(1,"Valentin);            
+
+-- CREATE TABLE SELECT id,name FROM user WHERE id == 1 ;
+
+-- DELETE FROM user WHERE id == 1;
+
+-- INSERT INTO user(id,name) VALUES (3, "Wena compañero") ;
+
+-- CREATE TABLE Hospital(
+--     id_hospt INT PRIMARY KEY,
+--     id_tipo INT,
+--     name VARCHAR(40),
+--     n_camas INT
+-- );
+
+-- CREATE TABLE Propio(
+--     id_tipo INT,
+--     presupuesto INT,
+--     servicio VARCHAR(40),
+--     FOREIGN KEY (id_tipo) REFERENCES Hospital(id_tipo)
+-- );
+CREATE TABLE Consecionado 
