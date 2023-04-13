b738d7dbdc33d47ba37998b94fb8310a566d48e6
charlotosisi <valentin.gaete@pregrado.uoh.cl>
2023-04-13 18:53:42 -0400

    cambios version 13/04/23

--- sql.sql
+++ sql.sql
@@ -1,64 +1,89 @@
 
-DROP TABLE Hospital ; 
-DROP TABLE HospitalPropio ; 
-DROP TABLE Poliza;
-DROP TABLE Asegurados;
--- DROP TABLE HospitalConcesionado ;
--- DROP TABLE Zonas;
--- DROP TABLE Medico;
+DROP TABLE IF EXISTS Hospital;
+DROP TABLE IF EXISTS HospitalPropio ;
+DROP TABLE IF EXISTS Poliza;
+DROP TABLE IF EXISTS Asegurados;
+DROP TABLE IF EXISTS HospitalConcesionado ;
+DROP TABLE IF EXISTS Zonas;
+DROP TABLE IF EXISTS CAT_P;
+DROP TABLE IF EXISTS CAT_S;
+DROP TABLE IF EXISTS Medico;
 
 
 -- CREACION DE TABLAS DE LAS ENTIDADES
--- Tabla entidad Hospital: 
+-- Tabla entidad Hospital: listo
 CREATE TABLE Hospital(
-    id_hospt INT PRIMARY KEY NOT NULL,
-    id_tipo INT NOT NULL,
+    codigo_hospt INT PRIMARY KEY NOT NULL,
     name_hospt VARCHAR(40) NOT NULL,
-    tipo_serv VARCHAR(40) NOT NULL
+    numero_camas INT NOT NULL,
+    tipo_servicio VARCHAR(40) NOT NULL -- (si es propio o concesionado)
+
 );
 
--- Tabla entidad Propio:
+-- Tabla entidad Hospital Propio:
 CREATE TABLE HospitalPropio(
     presupuesto INT NOT NULL,
     servicio VARCHAR(40) NOT NULL,
-    id_hospt INT PRIMARY KEY NOT NULL,
-    FOREIGN KEY (id_hospt) REFERENCES Hospital(id_hospt) IF 
+    codigo_hospt INT PRIMARY KEY NOT NULL,
+    FOREIGN KEY (codigo_hospt) REFERENCES Hospital(codigo_hospt) 
+
 );
+-- Tabla Hospital Concesionado
+CREATE TABLE HospitalConcesionado(
+    codigo_zona INT PRIMARY KEY NOT NULL,
+    codigo_hospt INT NOT NULL,
+    FOREIGN KEY (codigo_hospt) REFERENCES Hospital(codigo_hospt),
+    FOREIGN KEY (codigo_zona) REFERENCES Zonas(codigo_zona)
+
+);
+
 -- Tabla entidad Poliza: 
 CREATE TABLE Poliza(
     id_poliza INT PRIMARY KEY NOT NULL,
-    nombre_poli VARCHAR(40) NOT NULL,
-    name VARCHAR(40) NOT NULL
+    nombre_poliza VARCHAR(40) NOT NULL,
+    name_poliza VARCHAR(40) NOT NULL
     
 );
 
+-- Tabla Asegurados
 CREATE TABLE Asegurados(
+    id_asegurado INT PRIMARY KEY NOT NULL,
     id_poliza INT,
-    tipo_categoria VARCHAR(40),
+    nombre VARCHAR(40),
+    fecha_nacimiento VARCHAR(40),
+    categoria VARCHAR(40),
     FOREIGN KEY (id_poliza) REFERENCES Poliza(id_poliza)  
    
-
 );
-
+-- Tabla categoria primaria
 CREATE TABLE CAT_P(
-    id_hospt INT NOT NULL,
-    FOREIGN KEY (id_hospt) REFERENCES Hospital(id_hospt)
-
+    codigo_hospt INT NOT NULL,
+    FOREIGN KEY (codigo_hospt) REFERENCES Hospital(id_hospt)
 
 );
-
+-- Tabla categoria secundaria
 CREATE TABLE CAT_S(
     id_hospt int NOT NULL,
-    FOREIGN KEY (id_hospt) REFERENCES HospitalPropio(id_hospt),
+    codigo_zona INT NOT NULL,
+    FOREIGN KEY (codigo_zona) REFERENCES Zonas(codigo_zona),
+    FOREIGN KEY (id_hospt) REFERENCES HospitalPropio(id_hospt)
 
 );
+-- Tabla zonas
+CREATE TABLE Zonas(
+    codigo_zona INT PRIMARY KEY NOT NULL,
+    superficie INT NOT NULL,
+    num_habitantes INT NOT NULL
 
-
-CREATE TABLE HospitalConcesionado();
-
-CREATE TABLE Zonas();
-
-CREATE TABLE Medico();
+);
+-- Tabla Medico
+CREATE TABLE Medico(
+    id_medico INT PRIMARY KEY NOT NULL,
+    nombre VARCHAR(40) NOT NULL,
+    telefono_contacto INT NOT NULL,
+    id_jefe INT NOT NULL
+    -- FOREIGN (id_jefe) REFERENCES Medico(id_medico) 
+);
 
 -- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
